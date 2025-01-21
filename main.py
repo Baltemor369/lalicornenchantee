a8b6882fe3a223e208cf9da2d7c1aa8638ef117a
Baltemor369 <thomespo@gmail.com>
2025-01-21 13:11:10 +0100

    first matching distance and price

--- main.py
+++ main.py
@@ -2,32 +2,53 @@ import streamlit as st
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 
+# rayon premier cercle : 25km
+# rayon deuxième cercle : 50km
+# rayon troisième cercle : 75km
+# rayon quatrième cercle : 100km
+
+geolocator = Nominatim(user_agent="equiconfort")
+
 # Coordonnées des centres
-Centre1 = (49.14524698873021, 6.108990297508489)
-Centre2 = (48.55194848812558, 6.745914470401151)
+_ = geolocator.geocode("flavigny, 57130, France")
+Centre1 = (_.latitude, _.longitude)
+
+_ = geolocator.geocode("hablainville, 54120, France")
+Centre2 = (_.latitude, _.longitude)
 
 # Titre de l'application
-st.title('Tarifs et Distances des Villes')
+st.title('Equiselle - Tarif')
 
 # Entrée du nom de la ville
-nom_ville = st.text_input("Entrez le nom de la ville pour obtenir le tarif et calculer la distance:")
+nom_ville = st.text_input("Entrez le nom de la ville pour obtenir le tarif:")
 
 if nom_ville:
-    # tarif = villes_tarifs.get(nom_ville, "Ville non trouvée")
-    # st.write(f"Le tarif pour {nom_ville} est: {tarif} €")
-    
     # Géocoder la ville pour obtenir ses coordonnées
-    geolocator = Nominatim(user_agent="equiconfort")
-    location = geolocator.geocode(nom_ville)
-    
+    location = geolocator.geocode(nom_ville)    
     if location:
         coord_ville = (location.latitude, location.longitude)
-        st.write(f"Coordonnées de {nom_ville.capitalize()}: {coord_ville}")
         
         # Calculer les distances
         distance1 = geodesic(coord_ville, Centre1).km
         distance2 = geodesic(coord_ville, Centre2).km
-        st.write(f"Distance jusqu'à Centre1: {distance1:.2f} km")
-        st.write(f"Distance jusqu'à Centre2: {distance2:.2f} km")
+        # st.write(f"Distance jusqu'à Centre1: {distance1:.2f} km")
+        # st.write(f"Distance jusqu'à Centre2: {distance2:.2f} km")
+        
+        if nom_ville.lower() == "luxembourg":
+            tarif = 70 
+        else:
+            tarif = 0
+            if distance1 <= 25 or distance2 <= 25:
+                tarif = 60
+            elif distance1 <= 50 or distance2 <= 50:
+                tarif = 65
+            elif distance1 <= 75 or distance2 <= 75:
+                tarif = 70
+            elif distance2 <= 110:
+                tarif = 75
+            else:
+                tarif = "À négocier"
+            
+        st.write(f"Le tarif pour {nom_ville.capitalize()} est: {tarif} €")
     else:
         st.write("Impossible de géocoder la ville. Veuillez vérifier l'orthographe.")
